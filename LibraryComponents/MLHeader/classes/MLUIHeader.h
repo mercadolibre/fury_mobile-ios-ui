8a86b9eb5ae7d4761fa02aa5654c1a7a109d7e08
Cristian H. Gimenez <cristianh.gimenez@mercadolibre.com>
2019-03-14 16:41:59 -0300

    Uncrustify

--- LibraryComponents/MLHeader/classes/MLUIHeader.h
+++ LibraryComponents/MLHeader/classes/MLUIHeader.h
@@ -1,23 +1,23 @@
 //
-//  MLUIHeader.h
-//  MLUI
+// MLUIHeader.h
+// MLUI
 //
-//  Created by Cristian Gimenez on 12/03/2019.
+// Created by Cristian Gimenez on 12/03/2019.
 //
 
 #import <UIKit/UIKit.h>
 #import "MLUIHeaderDelegate.h"
 
 typedef NS_ENUM (NSInteger, MLUIHeaderMode) {
-    /**
-     *  The default mode. The header is shown, but is collapsed as the user reaches the top of the content.
-     */
-    MLUIHeaderModeDefault,
-    
-    /**
-     *  The header will not be visible. The navigation bar will be fixed on the screen.
-     */
-    MLUIHeaderModeAlwaysCollapsed
+	/**
+	 *  The default mode. The header is shown, but is collapsed as the user reaches the top of the content.
+	 */
+	MLUIHeaderModeDefault,
+
+	/**
+	 *  The header will not be visible. The navigation bar will be fixed on the screen.
+	 */
+	MLUIHeaderModeAlwaysCollapsed
 };
 
 @interface MLUIHeader : UIViewController
