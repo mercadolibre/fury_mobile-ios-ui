9ee8aec303c245f326f92e1f57ea6ac0ae87edf0
Ariel Goldfryd <ariel.goldfryd@mercadolibre.com>
2020-04-15 17:38:52 -0300

    Increase MLButton tap area using accessibility tools

--- LibraryComponents/MLButton/classes/MLButton.m
+++ LibraryComponents/MLButton/classes/MLButton.m
@@ -108,6 +108,9 @@ static const CGFloat kMLButtonSmallVerticalPadding = 11.0f;
 	[self.layer addSublayer:self.backgroundLayer];
 
 	self.backgroundLayer.borderWidth = kMLButtonBorderWidth;
+    
+    [self.label setIsAccessibilityElement:NO];
+    [self setAccessibilityTraits:UIAccessibilityTraitButton];
 
 	[self setUpWithSize];
 	[self setUpContentView];
@@ -156,6 +159,9 @@ static const CGFloat kMLButtonSmallVerticalPadding = 11.0f;
 	// TitleLabel Constraints
 	[self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-p@priority-[label]|" options:0 metrics:@{@"p" : @0, @"priority" : @999} views:@{@"label" : self.label}]];
 	[self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[label]|" options:0 metrics:0 views:@{@"label" : self.label}]];
+    
+    // This prevent Voice Over read "possible text" before accessibility value
+    [self setLabel:self.label];
 }
 
 - (void)setupIconView
@@ -262,6 +268,7 @@ static const CGFloat kMLButtonSmallVerticalPadding = 11.0f;
 {
     if (!self.isLoading) {
         [super setEnabled:enabled];
+        [self setIsAccessibilityElement:enabled];
         [self updateLookAndFeel];
 	}
 }
@@ -299,6 +306,7 @@ static const CGFloat kMLButtonSmallVerticalPadding = 11.0f;
     [attributedString addAttribute:NSParagraphStyleAttributeName value:paragraphStyle range:NSMakeRange(0, title.length)];
 
     self.label.attributedText = attributedString;
+    [self setAccessibilityValue:self.buttonTitle];
 }
 
 - (void)setConfig:(MLButtonConfig *)config
